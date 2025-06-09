5e96b318fa09b45a01a0817617de38ce5abda696
“loraklein” <lora.klein@utahtech.edu>
2025-06-09 11:22:31 -0600

    fixing deployment errors again

--- app/climbers/[id]/edit/page.tsx
+++ app/climbers/[id]/edit/page.tsx
@@ -1,7 +1,11 @@
-import { use } from 'react'
 import EditClimberForm from '../../../../components/EditClimberForm'
 
-export default function EditClimberPage({ params }: { params: { id: string } }) {
-  const id = use(Promise.resolve(params.id))
-  return <EditClimberForm id={id} />
+interface PageProps {
+  params: {
+    id: string
+  }
+}
+
+export default function EditClimberPage({ params }: PageProps) {
+  return <EditClimberForm id={params.id} />
 } 
